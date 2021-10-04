417d6edd194409bdbbe7ed41aef9a6b5750198d3
Fuad Rustamov <rustamoff1999@gmail.com>
2021-10-04 14:37:33 +0400

    Change image and fix troubles with landscape autolayout

--- SecondApp_with_ProgrammaticUI/ViewController.swift
+++ SecondApp_with_ProgrammaticUI/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 class ViewController: UIViewController {
 
     //let's avoid polluting viewDidLoad
-    let bearImageView: UIImageView = {
-        let image = UIImageView(image: #imageLiteral(resourceName: "bab-pic"))
+    let nikeImageView: UIImageView = {
+        let image = UIImageView(image: #imageLiteral(resourceName: "nikeLogo"))
         
+        image.contentMode = .scaleAspectFit
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()//Anonymous Closure(Here we are writing only those things that'll never change)
@@ -20,9 +21,19 @@ class ViewController: UIViewController {
     let descriptionTextView: UITextView = {
         let textView = UITextView()
         
+        //:**Creating custom attribute textView(text's attributes)**
+        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+        
+        //Add other text there
+        attributedText.append(NSAttributedString(string: "\n\n\nAre you ready for loads ands loads of fun? Don't wait any longer! We hope to see you in our stores soon.", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13), NSAttributedString.Key.foregroundColor:  UIColor.gray]))
+        
+        //:**Setting text's atttributes**
+        textView.attributedText = attributedText
+        
+        
         textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
-        textView.text = "Join us today in our fun and games"
+        //textView.font = UIFont.boldSystemFont(ofSize: 18)
+        //textView.text = "Join us today in our fun and games"
         textView.backgroundColor = .white
         textView.textColor = .black
         textView.textAlignment = .center
@@ -31,10 +42,21 @@ class ViewController: UIViewController {
         return textView
     }()
     
+    let topImageContainerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return containerView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
+        view.addSubview(topImageContainerView)
+        topImageContainerView.addSubview(nikeImageView)
+        
         setupLayout()
     }
 
@@ -42,15 +64,22 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         
         let layout = view.safeAreaLayoutGuide//SafeArea representation
-        bearImageView.centerXAnchor.constraint(equalTo: layout.centerXAnchor).isActive = true
-        bearImageView.topAnchor.constraint(equalTo: layout.topAnchor, constant: 100).isActive = true
-        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        nikeImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        nikeImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        nikeImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
         
-        descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 50).isActive = true
-        descriptionTextView.leadingAnchor.constraint(equalTo: layout.leadingAnchor, constant: 0).isActive = true
-        descriptionTextView.trailingAnchor.constraint(equalTo: layout.trailingAnchor, constant: 0).isActive = true
+//        nikeImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+//        nikeImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant: 50).isActive = true
+        descriptionTextView.leadingAnchor.constraint(equalTo: layout.leadingAnchor, constant: 24).isActive = true
+        descriptionTextView.trailingAnchor.constraint(equalTo: layout.trailingAnchor, constant: -24).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: layout.bottomAnchor, constant: 0).isActive = true
+        
+        topImageContainerView.heightAnchor.constraint(equalTo: layout.heightAnchor, multiplier: 0.5).isActive = true
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         
     }
 
