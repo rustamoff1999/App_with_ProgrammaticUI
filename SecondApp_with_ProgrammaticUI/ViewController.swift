8d131e64c3df43e773eec74a7336578cae6e7295
Fuad Rustamov <rustamoff1999@gmail.com>
2021-10-05 14:46:57 +0400

    Add button at the bottom

--- SecondApp_with_ProgrammaticUI/ViewController.swift
+++ SecondApp_with_ProgrammaticUI/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var layout: UILayoutGuide!
+    var bottomControlsStackView: UIStackView!
+    
     //let's avoid polluting viewDidLoad
     let nikeImageView: UIImageView = {
         let image = UIImageView(image: #imageLiteral(resourceName: "nikeLogo"))
@@ -22,10 +25,10 @@ class ViewController: UIViewController {
         let textView = UITextView()
         
         //:**Creating custom attribute textView(text's attributes)**
-        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+        let attributedText = NSMutableAttributedString(string: "Just do it.", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
         
         //Add other text there
-        attributedText.append(NSAttributedString(string: "\n\n\nAre you ready for loads ands loads of fun? Don't wait any longer! We hope to see you in our stores soon.", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13), NSAttributedString.Key.foregroundColor:  UIColor.gray]))
+        attributedText.append(NSAttributedString(string: "\nThe tenacity of athletes and their spirit of pushing through adversity will win out in the end, and a global pandemic won't stop sports.", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13), NSAttributedString.Key.foregroundColor:  UIColor.gray]))
         
         //:**Setting text's atttributes**
         textView.attributedText = attributedText
@@ -51,19 +54,69 @@ class ViewController: UIViewController {
         return containerView
     }()
     
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Prev", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .red
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Next", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .blue
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        layout = view.safeAreaLayoutGuide//SafeArea representation
         view.addSubview(descriptionTextView)
         view.addSubview(topImageContainerView)
-        topImageContainerView.addSubview(nikeImageView)
+        
+        setupBottomControls()
         
         setupLayout()
     }
 
+    func setupBottomControls() {
+//        view.addSubview(previousButton)
+        
+        bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, nextButton])
+        bottomControlsStackView.axis = .horizontal
+        bottomControlsStackView.distribution = .fillEqually
+        
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bottomControlsStackView)
+        //:**Constrainting StackView**
+        NSLayoutConstraint.activate([
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: layout.bottomAnchor, constant: 0),
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: layout.leadingAnchor, constant: 0),
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: layout.trailingAnchor, constant: 0),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        //:**Constrainting previousButton**
+//        NSLayoutConstraint.activate([
+//            previousButton.bottomAnchor.constraint(equalTo: layout.bottomAnchor, constant: 0),
+//            previousButton.leadingAnchor.constraint(equalTo: layout.leadingAnchor, constant: 0),
+//            previousButton.trailingAnchor.constraint(equalTo: layout.trailingAnchor, constant: 0),
+//            previousButton.heightAnchor.constraint(equalToConstant: 50)
+//        ])
+    }
+    
     private func setupLayout() {
         view.backgroundColor = .white
+        topImageContainerView.addSubview(nikeImageView)
         
-        let layout = view.safeAreaLayoutGuide//SafeArea representation
         nikeImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
         nikeImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
         nikeImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
@@ -74,7 +127,7 @@ class ViewController: UIViewController {
         descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant: 50).isActive = true
         descriptionTextView.leadingAnchor.constraint(equalTo: layout.leadingAnchor, constant: 24).isActive = true
         descriptionTextView.trailingAnchor.constraint(equalTo: layout.trailingAnchor, constant: -24).isActive = true
-        descriptionTextView.bottomAnchor.constraint(equalTo: layout.bottomAnchor, constant: 0).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: bottomControlsStackView.bottomAnchor, constant: 0).isActive = true
         
         topImageContainerView.heightAnchor.constraint(equalTo: layout.heightAnchor, multiplier: 0.5).isActive = true
         topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
